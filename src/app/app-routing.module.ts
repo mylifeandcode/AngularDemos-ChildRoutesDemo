8f039718aaf3a45a61b03be58ec11a3749dbb00d
Alan Hummel <ahummel@abim.org>
2023-02-23 13:52:33 -0500

    Initial coding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OneComponent } from './one/one.component';
+import { ThreeComponent } from './three/three.component';
+import { TwoComponent } from './two/two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'one', 
+        component: OneComponent, 
+      },
+      {
+        path: 'two',
+        component: TwoComponent, 
+      },
+      {
+        path: 'three',
+        component: ThreeComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent
   ],
   imports: [
     BrowserModule,
